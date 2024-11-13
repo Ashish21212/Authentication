b93fb7ab9f3be4cea5c47775a075b28a98fc0dda
Ashish21212 <acp.aashish44@gmail.com>
2024-11-13 20:08:29 +0545

    views added

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const express = require("express");
+// const path = require('path');
+
+const express = require('express');
+
+
+
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/user');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(adminRoutes)
+app.use(userRoutes)
+
+
+
+
+app.listen(4000)
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+
+const router = express.Router();
+
+// router.get('/homepage',(req,res,next)=>{
+//     res.render('Hello');
+// })
+module.exports=router;
